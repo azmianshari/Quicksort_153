74db8db9c52b609be16c4bbe1e4c7b94d8480698
azmianshari <azmianshari123@gmail.com>
2023-03-31 10:59:11 +0700

    Membuat Array dengan batas panjang 20 element

--- Quicksort/Quicksort.cpp
+++ Quicksort/Quicksort.cpp
@@ -3,4 +3,19 @@ using namespace std;
 
 int arr[20];
 int cup_count = 0;
-int mov
+int mov_count = 0;
+int n;
+void input() {
+	while (true)
+	{
+		cout << "Masukan Panjang Element Array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else
+			cout << "Maksimum Panjang Array adalah 20" << endl;
+	}
+	cout << "===================" << endl;
+	cout << "Enter Array Element" << endl;
+	cout << "===================" << endl;
+}
