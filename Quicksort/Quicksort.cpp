773b5a9aee389974f2e443373b74466aca2bb9fb
azmianshari <azmianshari123@gmail.com>
2023-03-31 16:17:35 +0700

    J

--- Quicksort/Quicksort.cpp
+++ Quicksort/Quicksort.cpp
@@ -79,7 +79,7 @@ void q_short(int low, int high)
     // j now containt the index of the last element in the sorted list
     if (low < j)                                                            // Langkah algoritma No. 11
     {
-        // move the pivot to its correct position in the list
+        
         swap(low, j);
         mov_count++;
     }
