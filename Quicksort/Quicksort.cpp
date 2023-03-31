a103885b74bade2d5f968358f1353a238c8ab940
azmianshari <azmianshari123@gmail.com>
2023-03-31 16:13:59 +0700

    P

--- Quicksort/Quicksort.cpp
+++ Quicksort/Quicksort.cpp
@@ -108,7 +108,7 @@ void display() {
 int main()
 {
     input();
-    q_short(0, n - 1);                                                      // Sort the array using quick sort 
+    q_short(0, n - 1);                                                     
     display();
     system("pause");
 
