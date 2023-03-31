e64e81690017214f0a2a888393a37cc14c48967d
azmianshari <azmianshari123@gmail.com>
2023-03-31 16:16:27 +0700

    N

--- Quicksort/Quicksort.cpp
+++ Quicksort/Quicksort.cpp
@@ -83,7 +83,7 @@ void q_short(int low, int high)
         swap(low, j);
         mov_count++;
     }
-    // sort the list on the left pivot using quick sort
+    
     q_short(low, j - 1);                                                    // Langkah algoritma No. 12
 
     
